a40869640851f6aea04593fed3b34859277bff4e
anikaatia-cse <anikaatia23@gmail.com>
2026-07-08 22:12:41 +0600

    Completed Lab Assignment 1.

---
+++ LAB ASSIGNMENTS (BASICS)/assignment_1.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+/*
+Here #include<stdio.h> is a prepocessor command that 
+tells the compiler to include the standard input-output
+header file before compiling the  program.
+the standard input file is       printf()
+the standard output file is      scanf()
+*/
+
+/* we use slash and star like this when we want to
+   write multi-line comments in C program and that
+   tells the compiler to ignore the texts inside it. */
+
+// Write a C program that will print your name.   (we use double slash when it's only a single line comment.)
+
+
+int main()
+{
+    printf("My name is Anika Atia");
+    return 0;  // return 0; means the program will exit successfully.
+}
+
