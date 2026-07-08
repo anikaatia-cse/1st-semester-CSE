16a946386216435b9fc4c240a1534e7c7d6f49d0
anikaatia-cse <anikaatia23@gmail.com>
2026-07-08 22:13:17 +0600

    Completed Lab Assignment 2.

---
+++ LAB ASSIGNMENTS (BASICS)/assignment_2.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+/* Write a C program that will print your name, your father 
+   and mother name in three separate lines. */
+
+int main()
+{
+    printf("My name is Anika Atia\n");    // Here \n helps to print the next output in a new line.
+    printf("My father's name is Muhamed Mamunul Islam\n");
+    printf("My mother's name is Shamim Ara Begum\n");
+    return 0;
+}
