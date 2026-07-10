41c655a8524620104cb0dae98d260ac6ebd31aa2
anikaatia-cse <anikaatia23@gmail.com>
2026-07-10 22:42:49 +0600

    Completed Lab Assignment 5.

---
+++ LAB ASSIGNMENTS (BASICS)/assignment_5.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+/* Write a C program that will take three numbers
+   from user and find their average. */
+
+int main()
+{
+    int a, b, c, avg;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    avg = (a + b + c) / 3;
+    printf("The average of three numbers is: %d\n", avg);
+    return 0;
+}   
+
